0d5a192299e6126d325afe9a59b92a0467a65e65
Anny Cheng <dance790512@gmail.com>
2018-08-29 05:00:00 -0700

    add projects section

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
-
+    $('.about-button').click(function() {
+        $('html, body').animate({ scrollTop: $('#about').offset().top }, 800);
+    })
+    $('.project-button').click(function() {
+        $('html, body').animate({ scrollTop: $('#project').offset().top }, 800);
+    })
 });
  
